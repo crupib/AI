63a61177f29cdbecf5aadfc2a7f1abb2514998fb
William Crupi <crupib@gmail.com>
2025-05-05 10:25:43 -0400

    update mathops

--- pytorch/mathops.py
+++ pytorch/mathops.py
@@ -5,13 +5,22 @@ else:
    device = torch.device('cpu')
 x = torch.tensor([1,2,3])
 y = torch.tensor([4,5,6])
+print(x)
+print(y)
 addtion = x + y
 print(f"Addtion:\n {addtion}")
+print(x)
+print(y)
 subtraction = x - y
 print(f"subtraction:\n {subtraction}")
+print(x)
+print(y)
 multiplication = x * y
 print(f"multiplication:\n {multiplication}")
+print(x)
+print(y)
 division = x / y
 print(f"division:\n {division}")
+print(x)
 expo = x**2
 print(f"expo:\n {expo}")
